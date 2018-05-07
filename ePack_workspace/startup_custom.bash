3be372f42b05da94298e29c23d67c7b07e632eac
sammtcbn <sammtcbn@gmail.com>
2018-05-07 16:25:56 +0800

    remove useless var for ePack

--- ePack_workspace/startup_custom.bash
+++ ePack_workspace/startup_custom.bash
@@ -4,39 +4,23 @@
 PROJECT_NAME=API-GW
 INS_DIR=/usr/local/EdgeSense/${PROJECT_NAME}
 SERVICE_NAME=${PROJECT_NAME}.service
-#CONF_FILE_NAME=settings.ini
-#CONF_FILE_PATH=${INS_DIR}/${CONF_FILE_NAME}
 
 function install_dependency ()
 {
-    # Custom Section
-    echo "install_dependency"
+    return
 }
 
 function backup_config ()
 {
-    # Custom Section
-    echo "backup_config"
-#    rm -f /tmp/${CONF_FILE_NAME} || exit 1
-#    if [ -f $CONF_FILE_PATH ]; then
-#        echo "backup $CONF_FILE_PATH to /tmp"
-#        cp -f $CONF_FILE_PATH /tmp || exit 1
-#    fi
+    return
 }
 
 function restore_config ()
 {
-    # Custom Section
-    echo "restore_config"
-#    if [ -f /tmp/$CONF_FILE_NAME ]; then
-#        echo "restore $CONF_FILE_NAME to $CONF_FILE_PATH"
-#        mv -f /tmp/$CONF_FILE_NAME $CONF_FILE_PATH || exit 1
-#    fi
+    return
 }
 
 function install_others ()
 {
-    # Custom Section
-    echo "install_others"
+    return
 }
-
