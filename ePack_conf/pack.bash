3f8afaa8b2a6534dca54cae370af861350b7f7b2
sammtcbn <sammtcbn@gmail.com>
2018-05-09 00:18:49 +0800

    change pack folder for ePack

--- ePack_conf/pack.bash
+++ ePack_conf/pack.bash
@@ -9,7 +9,7 @@ fi
 cp -f ePack_custom.bash startup_custom.bash ${EPACK_PATH} || exit 1
 
 cd ..
-make install DESTDIR=${EPACK_PATH}/rootfs || exit 1
+make install DESTDIR=${EPACK_PATH}/archive/rootfs || exit 1
 
 cd ${EPACK_PATH} || exit 1
 ./build.bash
