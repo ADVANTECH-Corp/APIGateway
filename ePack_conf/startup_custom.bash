5c19f078b478c2d1f88c08c26d581d18ad2d9cdc
sammtcbn <sammtcbn@gmail.com>
2018-05-10 00:20:30 +0800

    change install folder for ePack_custom.bash

--- ePack_conf/startup_custom.bash
+++ ePack_conf/startup_custom.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Custom Section
-PROJECT_NAME=API-GW
 INS_DIR=/usr/local/EdgeSense/${PROJECT_NAME}
-SERVICE_NAME=${PROJECT_NAME}.service
 
 function install_dependency ()
 {
--- pack.bash
+++ pack.bash
@@ -6,7 +6,7 @@ if [ -z "${EPACK_PATH}" ]; then
     exit 1
 fi
 
-cp -f ePack_conf/ePack_custom.bash ${EPACK_PATH} || exit 1
+cp -f ePack_conf/ePack_custom.bash ${EPACK_PATH}/archive || exit 1
 cp -f ePack_conf/startup_custom.bash ${EPACK_PATH}/archive || exit 1
 
 make install DESTDIR=${EPACK_PATH}/archive/rootfs || exit 1
